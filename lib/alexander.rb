e466702430cf3571eb4328cb4490ca9ae9886070
jproyo <juanpablo.royo@gmail.com>
2012-05-14 16:28:26 -0300

    Adding create command

--- lib/alexander.rb
+++ lib/alexander.rb
@@ -1,7 +1,13 @@
 require 'fileutils'
 
 module Alexander
+
 	class AppBase
+		
+		def self.run(file_path)
+			AppBase.send(ARGV[0].to_sym, ARGV[1], file_path)
+		end
+
 		def self.create(application, base_path)
 			dest = File.join(base_path, application)
 			Dir.mkdir(dest)
